53b1c7919892f999a5f910f8f73405c29932e039
Vladimir Aleksic <vladimiraleksic.nis@gmail.com>
2024-04-16 14:00:39 +0000

    add Book model in apps models.py

--- berlin_project/models.py
+++ berlin_project/models.py
@@ -2,3 +2,12 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Book(models.Model):
+	title = models.ForeignKey(Review, on_delete=models.CASCADE, related_name="book_reviews")
+	author = models.CharField(max_length=150)
+	publisher = models.CharField(max_length=100)
+	year = models.IntegerField()
+
+	def __str__(self):
+    	return self.title.content
+
