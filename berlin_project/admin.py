0470dacb9e7919014f3b033d3d05792d271931d1
Vladimir Aleksic <vladimiraleksic.nis@gmail.com>
2024-04-16 14:25:45 +0000

    create Book and Review models

--- berlin_project/admin.py
+++ berlin_project/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Book, Review
 
 # Register your models here.
+admin.site.register(Post)
+admin.site.register(Review)
--- berlin_project/models.py
+++ berlin_project/models.py
@@ -2,12 +2,21 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class Book(models.Model):
-	title = models.ForeignKey(Review, on_delete=models.CASCADE, related_name="book_reviews")
-	author = models.CharField(max_length=150)
-	publisher = models.CharField(max_length=100)
-	year = models.IntegerField()
 
-	def __str__(self):
-    	return self.title.content
+class Review(models.Model):
+    content = models.TextField(max_length=500, unique=True)
+
+    def __str__(self):
+        return self.content
+
+class Book(models.Model):
+    title = models.ForeignKey(Review, on_delete=models.CASCADE, related_name="book_reviews")
+    author = models.CharField(max_length=150)
+    publisher = models.CharField(max_length=100)
+    year = models.IntegerField()
+
+    def __str__(self):
+        return self.title.content
+
+
 
