903c52f1b3d717c97fb5aa197590c07ebfb52d83
Vladimir Aleksic <vladimiraleksic.nis@gmail.com>
2024-04-18 09:02:04 +0000

    refactor models.py

--- berlin_project/models.py
+++ berlin_project/models.py
@@ -3,21 +3,22 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-class Review(models.Model):
-    content = models.TextField(max_length=500, unique=True)
-
-    def __str__(self):
-        return self.content
-
 class Book(models.Model):
     title = models.CharField(max_length=150)
     author = models.CharField(max_length=150)
     publisher = models.CharField(max_length=100)
     year = models.IntegerField()
-    review = models.ForeignKey(Review, on_delete=models.CASCADE, related_name="books", null=True, blank=True)
+    summary = models.ForeignKey('Review', on_delete=models.CASCADE, related_name="books", null=True, blank=True)
 
     def __str__(self):
         return self.title
 
+    class Meta:
+        ordering = ['title']
 
+class Review(models.Model):
+    content = models.TextField(max_length=500, unique=True)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f"You are reading the critique of the {self.book.title} - {self.content[:50]}"
