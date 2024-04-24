f43fc19993d93813d45cd80bbe8a0352882ab8fc
Vladimir Aleksic <vladimiraleksic.nis@gmail.com>
2024-04-24 15:20:57 +0000

    Revert "delete all migrations and refactor model"
    This reverts commit 861a61708689d1192cb334e7f4def16f9c43c183.

--- berlin_project/models.py
+++ berlin_project/models.py
@@ -4,11 +4,11 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Book(models.Model):
-    title = models.CharField(max_length=150, unique=True, blank=False)
-    author = models.CharField(max_length=150, blank=False)
+    title = models.CharField(max_length=150)
+    author = models.CharField(max_length=150)
     publisher = models.CharField(max_length=100)
     year = models.IntegerField()
-    description = models.TextField(blank=False)
+    summary = models.ForeignKey('Review', on_delete=models.CASCADE, related_name="books", null=True)
 
     def __str__(self):
         return self.title
@@ -17,8 +17,8 @@ class Book(models.Model):
         ordering = ['title']
 
 class Review(models.Model):
-    content = models.TextField(max_length=500)
-    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='reviews')
+    content = models.TextField(max_length=500, unique=True)
+    book_title = models.ForeignKey(Book, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
-        return f"Review of '{self.book.title}' - {self.content[:50]}"
+        return f"You are reading the critique of the {self.book_title.title} - {self.content[:50]}"
